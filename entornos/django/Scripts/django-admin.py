51f5ea843f8b820f1d30cbc4f26ba1dfa7b5b031
Ane Miren Echeverria <aneecheverria11@gmail.com>
2020-04-24 10:02:46 +0200

    cambios¿?

---
+++ entornos/django/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\Anelo\Documents\clase\2019-2020\C2\SISTEMAS WEB\entornos\django\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
